e76e91d9ef177e09b6adbc23943ac2d92d0b51be
Chris Tybur <thisiscmt@gmail.com>
2023-02-21 21:34:49 -0800

    Env variable updates

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: 'http://localhost:3050'
+    origin: process.env.DIHT_ALLOWED_ORIGIN
 }));
 
 app.use('/', baseRouter);
--- src/middleware/authChecker.ts
+++ src/middleware/authChecker.ts
@@ -2,7 +2,7 @@ import {NextFunction, Request, Response} from 'express';
 
 const authChecker = (request: Request, response: Response, next: NextFunction) => {
     try {
-        if (request.headers['x-diht-agent'] === undefined || request.headers['x-diht-agent'] !== process.env.DIHT_Agent_Header) {
+        if (request.headers['x-diht-agent'] === undefined || request.headers['x-diht-agent'] !== process.env.DIHT_ALLOWED_USER_AGENT) {
             return response.status(403).send('The request is not authorized');
         }
     } catch (error) {
